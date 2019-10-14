b1f82d73929f723bbe1ef5de25413362f56fc7bd
xps3riments <xps3riments@gmail.com>
2019-10-14 22:46:00 +0200

    Initial commit

---
+++ build.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+const fetch = require("node-fetch");
+const saveFilePaths = "public";
+function downloadUser(user) {
+  return fetch("https://www.tweetjs.com/API.aspx", {
+    credentials: "include",
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+      "cache-control": "no-cache",
+      "content-type": "text/plain;charset=UTF-8",
+      pragma: "no-cache"
+    },
+    referrer: "https://www.tweetjs.com/ListTweetsOnUserTimeline.html",
+    referrerPolicy: "no-referrer-when-downgrade",
+    body: `{"Action":"ListTweetsOnUserTimeline","ScreenName":"${user}"}`,
+    method: "POST",
+    mode: "cors"
+  })
+    .then(r => r.text())
+    .then(body => {
+      fs.writeFileSync(
+        path.resolve(__dirname, saveFilePaths, `${user}.json`),
+        body
+      );
+      return body;
+    });
+}
+
+Promise.all(["xps3riments", "psxplace"].map(downloadUser)).then(() => {
+  console.log("done");
+});
+
+fs.writeFileSync(
+  path.resolve(__dirname, saveFilePaths, "index.html"),
+  "HELLOWORLD"
+);
